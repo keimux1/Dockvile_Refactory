c1739e0f46a207dc9fb4c69f1dec6bbac448e39f
Emulu Keith <keimux21@gmail.com>
2023-08-15 23:35:28 +0300

    changed to an mvc formart and fixed my pug css

---
+++ config/dbConfig.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const conneectDB = async ()=>{
+  try{
+    conn = await mongoose.connect(process.env.MONGO_URI)
+    console.log(`MOngoDB connected at: ${conn.connection.host}`)
+    // Or console.log("MOngoDB connected at: "+conn.connection.host)
+}
+catch(error){
+  console.log("MOngoDB connected at: "+error)
+  process.exit(1)
+}
+}
+
+module.exports = conneectDB;
--- server.js
+++ server.js
@@ -1,2 +1,22 @@
-const express = require("express")
-const path = require("path")
+const express = require("express");
+const path = require("path");
+const port = process.env.PORT || 3000
+const app = express();
+
+app.engine("pug", require("pug").__express)
+app.set("view engine", "pug");
+app.set("veiws", path.join(__dirname,"views"))
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get ("/battery", (req, res) => {
+    res.render("Battery purchase");
+})
+
+
+
+
+
+app.listen(port,()=>{
+    console.log("Server is running on port "+port);
+});
